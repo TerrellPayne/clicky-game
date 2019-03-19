90b97c835b9d2420f55e910b7087e85ba18990df
TerrellPayne <44079103+TerrellPayne@users.noreply.github.com>
2019-03-19 19:58:07 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Score from "./components/Score";
+import friends from "./friends.json";
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends,
+    clickedFriendIds: [],
+    score: 0,
+    goal: 10,
+    status: ""
+  };
+
+  shuffleScoreCard = id => {
+    let clickedFriendIds = this.state.clickedFriendIds;
+
+    if(clickedFriendIds.includes(id)){
+      this.setState({ clickedFriendIds: [], score: 0, status:  "Game Over! You lost. Click to play again!" });
+      return;
+    }else{
+      clickedFriendIds.push(id)
+
+      if(clickedFriendIds.length === 8){
+        this.setState({score: 8, status: "You Won! Click to play again!", clickedFriendIds: []});
+        console.log('You Win. Stay Sharp, Pal!');
+        return;
+      }
+
+      this.setState({ friends, clickedFriendIds, score: clickedFriendIds.length, status: " " });
+
+      for (let i = friends.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [friends[i], friends[j]] = [friends[j], friends[i]];
+      }
+    }
+  }
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Test your Memory</h1>
+        <p className="App-intro">
+          Try not to click the same image twice!
+        </p>
+      </header>
+      <Score total={this.state.score}
+              goal={10}
+              status={this.state.status}
+      />
+      <Wrapper>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            shuffleScoreCard={this.shuffleScoreCard}
+            // removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            // occupation={friend.occupation}
+            // location={friend.location}
+          />
+        ))}
+      </Wrapper>
+      <footer>
+      
+    </footer>
+    </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
